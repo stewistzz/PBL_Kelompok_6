9ec7f2d261bf99b7b33e507d16311c6a993b02ea
hauraarchia <hauraarchia@gmail.com>
2024-12-03 23:19:47 +0700

    menambahkan header untuk navbar

---
+++ layouts/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Navbar & Tabs</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../adminlte/plugins/fontawesome-free/css/all.min.css">
+    <!-- SweetAlert2 -->
+    <link rel="stylesheet" href="../adminlte/plugins/sweetalert2/sweetalert2.min.css">
+    <!-- Toastr -->
+    <link rel="stylesheet" href="../adminlte/plugins/toastr/toastr.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../adminlte/dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini" style="background-image: url(img/bg.jpeg);  overflow: hidden; /* Mencegah scroll */
+            height: 100vh; /* Mengatur tinggi body */
+            display: flex; /* Menggunakan Flexbox */
+            justify-content: center; /* Menempatkan konten di tengah secara horizontal */
+            align-items: center; /* Menempatkan konten di tengah secara vertikal */
+            background-image: url(img/bg.jpeg);
+            background-repeat: no-repeat;
+            background-size: cover; ">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand navbar-primary navbar-dark fixed-top">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><img src="img/logo_poltek.png" alt="" width="100px"></i></a>
+                </li>
+                <li class="nav-item">
+                    <p><br>JURUSAN <br> <b><u>TEKNOLOGI INFORMASI</u></b> <br> POLITEKNIK NEGERI MALANG</p>
+                </li>
+            </ul>
+
+            <!-- Right navbar links -->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><img src="img/jti.png" alt="" width="100px"></i></a>
+                </li>
+            </ul>
+        </nav>
+
+
+        <!-- /.navbar -->
+    </div>
+    <!-- ./wrapper -->
+    <div class="navbar-line" style="background-color: #ffd700; /* Warna kuning sesuai gambar */
+        height: 5px;
+        width: 500%;">
+    </div>
+    <!-- jQuery -->
+    <script src="../adminlte/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../adminlte/dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../adminlte/dist/js/demo.js"></script>
+</body>
+
+</html>
