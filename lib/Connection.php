6a15cc6abefa191651369a4d27b4b24fe03b34a3
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-12-05 20:28:54 +0700

    modifikasi connection

--- lib/Connection.php
+++ lib/Connection.php
@@ -1,12 +1,32 @@
 <?php
-$username = 'root';
+$use_driver = 'sqlsrv'; // mysql atau sqlsrv
+$host = 'localhost';
+$username = ''; //'sa';
 $password = '';
-$database = 'dasar_web';
-try {
-    $db = new mysqli('localhost', $username, $password, $database);
-    if ($db->connect_error) {
-        die('Connection DB failed: ' . $db->connect_error);
+$database = 'bebas_tanggungan';
+$db;
+if ($use_driver == 'mysql') {
+    try {
+        $db = new mysqli('localhost', $username, $password, $database);
+        if ($db->connect_error) {
+            die('Connection DB failed: ' . $db->connect_error);
+        }
+    } catch (Exception $e) {
+        die($e->getMessage());
     }
-} catch (Exception $e) {
-    die($e->getMessage());
+} else if ($use_driver == 'sqlsrv') {
+    $credential = [
+        'Database' => $database,
+        'UID' => $username,
+        'PWD' => $password
+    ];
+    try {
+        $db = sqlsrv_connect($host, $credential);
+        if (!$db) {
+            $msg = sqlsrv_errors();
+            die($msg[0]['message']);
+        }
+    } catch (Exception $e) {
+        die($e->getMessage());
+    }
 }
