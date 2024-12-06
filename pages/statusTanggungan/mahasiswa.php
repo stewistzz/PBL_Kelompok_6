0cbcda1c6d0ea96373ee3ae06823982cacfea90d
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-12-07 05:28:14 +0700

    add_pages_bebasTanggungan admin dan mahasiswa

---
+++ pages/statusTanggungan/mahasiswa.php
@@ -0,0 +1,58 @@
+
+<section class="content-header">
+                 <!-- toggle -->
+                 <?php include('layouts/toggle.php'); ?>
+                 <!-- toggle -->
+                 <div class="container-fluid">
+                     <div class="row mb-2">
+                         <div class="col-sm-12">
+                             <h1 class="text-center font-weight-bold">Surat Keterangan Mahasiswa</h1>
+                         </div>
+                     </div>
+                 </div>
+             </section>
+
+<div class="header">
+    <h3>Mahasiswa view</h3>
+</div>
+
+
+<section class="content">
+                <div class="page-inner">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="list-group">
+                                        <div class="list-group-item d-flex justify-content-between align-items-center">
+                                            Pembayaran UKT
+                                            <span class="badge badge-success">Sudah Terbayarkan</span>
+                                        </div>
+                                        <div class="list-group-item d-flex justify-content-between align-items-center">
+                                            Laporan Tugas Akhir / Skripsi
+                                            <span class="badge badge-success">Sudah Terpenuhi</span>
+                                        </div>
+                                        <div class="list-group-item d-flex justify-content-between align-items-center">
+                                            Surat Bebas Kompen
+                                            <span class="badge badge-success">Sudah Terpenuhi</span>
+                                        </div>
+                                        <div class="list-group-item d-flex justify-content-between align-items-center">
+                                            Surat Bebas Peminjaman
+                                            <span class="badge badge-success">Sudah Terpenuhi</span>
+                                        </div>
+                                        <div class="list-group-item d-flex justify-content-between align-items-center">
+                                            Sertifikat TOEIC
+                                            <span class="badge badge-danger">Belum Terpenuhi</span>
+                                        </div>
+                                        <div class="list-group-item d-flex justify-content-between align-items-center">
+                                            SKKM
+                                            <span class="badge badge-success">Sudah Terpenuhi</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
