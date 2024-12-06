3da1087e547e7701eb7b1048b0417b57277bcf56
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-12-07 05:32:03 +0700

    pages cetak tanggungan untuk mahasiswa

---
+++ pages/cetakTanggungan/mahasiswa.php
@@ -0,0 +1,13 @@
+<section class="content-header">
+    <!-- toggle -->
+    <?php include('layouts/toggle.php'); ?>
+    <!-- toggle -->
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-12">
+                <h1 class="text-center font-weight-bold">Surat Keterangan Mahasiswa</h1>
+            </div>
+        </div>
+    </div>
+</section>
+<h3>Cetak Tanggungan Mahasiswa</h3>
