df83c511fc88422629df5939220442aa799e2696
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-12-07 05:29:40 +0700

    add_folder_structure untuk template header dan footer

---
+++ structure/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Admin</title>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="adminlte/plugins/fontawesome-free/css/all.min.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="adminlte/plugins/datatablesbs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="adminlte/plugins/datatablesresponsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="adminlte/plugins/datatablesbuttons/css/buttons.bootstrap4.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="adminlte/dist/css/adminlte.min.css">
+
+    <!-- Theme style -->
+    <link rel="stylesheet" href="assets/css/dashboardAdmin.css">
+    <!-- jQuery -->
+    <script src="adminlte/plugins/jquery/jquery.min.js"></script>
+    <style>
+        .content-header {
+            /* margin-top: 20px; */
+            /* margin-bottom: 10px */
+        }
+    </style>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <?php include('layouts/header.php'); ?>
+    <div class="wrapper">
+        <!-- Main Sidebar Container -->
+        <?php include('layouts/sidebar.php');?>
+    </div>
+
+    <!-- Content Wrapper -->
+    <div class="content-wrapper">
+        <!-- Content Header -->
+        <!-- Main Content -->
