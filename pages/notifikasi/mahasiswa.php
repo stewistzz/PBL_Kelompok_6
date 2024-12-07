120506f4fe4e4ec61d065f1a0cd03d6048f72d37
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-12-07 22:13:20 +0700

    modif notifikasi mahasiswa

--- pages/notifikasi/mahasiswa.php
+++ pages/notifikasi/mahasiswa.php
@@ -1,50 +1,29 @@
+<style>
+    
+    hr  {
+        height: 10px; 
+    }
+</style>
+
+
+<!-- searching section -->
 <section class="content-header">
     <!-- toggle -->
     <?php include('layouts/toggle.php'); ?>
     <!-- toggle -->
     <div class="container-fluid">
         <div class="row mb-2">
-            <div class="col-sm-12">
-                <h1 class="text-center font-weight-bold">Surat Keterangan Mahasiswa</h1>
+            <div class="input-group mb-3 w-50 m-auto">
+                <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-primary" type="button" id="button-addon2">Button</button>
+                </div>
             </div>
         </div>
     </div>
 </section>
+
+<hr>
 
 <!-- Notifikasi Mahasiswa -->
-<section class="notifications">
-    <h3>Notif Mahasiswa</h3>
-    
-    <!-- Menambahkan Gambar Dosen -->
-    <div class="dosen-image">
-        <img src="pages/notifikasi/image/dosenn.jpg" alt="Dosen" class="img-fluid" style="max-width: 200px; margin: 10px auto; display: block;">
-    </div>
-
-    <div class="notification-list">
-        <div class="notification">
-            <!-- Gambar Profil Admin -->
-            <img src="avatar.png" alt="Admin" class="avatar">
-            <div class="text">
-                <h4>Admin</h4>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-            </div>
-            <span class="badge">!</span>
-        </div>
-        <div class="notification">
-            <img src="avatar.png" alt="Admin" class="avatar">
-            <div class="text">
-                <h4>Admin</h4>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-            </div>
-            <span class="badge">!</span>
-        </div>
-        <div class="notification">
-            <img src="avatar.png" alt="Admin" class="avatar">
-            <div class="text">
-                <h4>Admin</h4>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-            </div>
-            <span class="badge">!</span>
-        </div>
-    </div>
-</section>
+<?php include('components/detail.php')?>
