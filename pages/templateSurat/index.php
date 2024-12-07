7eacb30dad310921998a47aebc38b6d5b2652c6d
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-12-07 22:11:44 +0700

    add fitur download template

---
+++ pages/templateSurat/index.php
@@ -0,0 +1,90 @@
+<section class="content-header">
+                 <!-- toggle -->
+                 <?php include('layouts/toggle.php'); ?>
+                 <!-- toggle -->
+                 <div class="container-fluid">
+                     <div class="row mb-2">
+                         <div class="col-sm-12">
+                             <h1 class="text-center font-weight-bold">Template Dokumen Bebas Tanggungan</h1>
+                         </div>
+                     </div>
+                 </div>
+             </section>
+
+             <hr>
+
+<!-- template surat -->
+<div class="container pt-3 ms-3 m-auto">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/img/bebas kompen.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><b>Bebas Kompen</b></h5>
+                    <p class="card-text">Template untuk download surat bebas kompen mahasiswa JTI</p>
+                    <a href="#" class="btn btn-primary">Donwload</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/img/terima laporan TA.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><b>TANDA TERIMA LAPORAN TUGAS AKHIR</b></h5>
+                    <p class="card-text">Template untuk download surat bebas kompen mahasiswa JTI</p>
+                    <a href="#" class="btn btn-primary">Donwload</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/img/terima laporan pkl.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><b>TANDA TERIMA LAPORAN PKL</b></h5>
+                    <p class="card-text">Template untuk download surat bebas kompen mahasiswa JTI</p>
+                    <a href="#" class="btn btn-primary">Donwload</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/img/kebenaran data diri.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><b>PERNYATAAN KEBENARAN DATA DIRI</b></h5>
+                    <p class="card-text">Template untuk download surat bebas kompen mahasiswa JTI</p>
+                    <a href="#" class="btn btn-primary">Donwload</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/img/bebas tanggungan jurusan.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><b>BEBAS TANGGUNGAN JURUSAN</b></h5>
+                    <p class="card-text">Template untuk download surat bebas kompen mahasiswa JTI</p>
+                    <a href="#" class="btn btn-primary">Donwload</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/img/rekomendasi pengambilan transkrip.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><b>REKOMENDASI PENGAMBILAN TRANSKRIP, IJAZAH DAN SKPI</b></h5>
+                    <p class="card-text">Template untuk download surat bebas kompen mahasiswa JTI</p>
+                    <a href="#" class="btn btn-primary">Donwload</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img src="pernyataan dan jaminan publikasi.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><b>SURAT PERNYATAAN DAN JAMINAN PUBLIKASI TUGAS AKHIR MAHASISWA </b></h5>
+                    <p class="card-text">Template untuk download surat bebas kompen mahasiswa JTI</p>
+                    <a href="#" class="btn btn-primary">Donwload</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
