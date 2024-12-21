91506a3235be4b9986b6644520cef616d1d4b68f
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-12-21 10:50:44 +0700

    memperbaiki layouts

--- layouts/sidebarAdmin.php
+++ layouts/sidebarAdmin.php
@@ -5,7 +5,7 @@
             <img src="adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-            <a href="#" class="d-block">Alexander Pierce</a>
+            <a href="pages/profile/profile.php" class="d-block">Alexander Pierce</a>
         </div>
     </div>
 
